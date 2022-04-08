821afe531823bf17a1912cc4c66667e682a161e4
Nate Baker <streetcodernate@Nates-MacBook-Air.local>
2022-04-07 21:54:31 -0400

    app server created

---
+++ app.py
@@ -0,0 +1,22 @@
+from botting_nigga import ask, append_interaction_to_chat_log
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, session
+app = Flask(__name__)
+# if for some reason your conversation with Jabe gets weird, change the secret key
+app.config['SECRET_KEY'] = '0.848436602531039'
+
+
+@app.route('/bottn_nigga', methods=['POST'])
+def bottn_nigga():
+    incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                         chat_log)
+    msg = MessagingResponse()
+    msg.message(answer)
+    return str(msg)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
