6371b1e270eae2fb58b1681f7baad260b0058b10
Nate Baker <natebaker@Nates-MacBook-Pro.local>
2022-10-25 16:16:32 -0400

    removed the dependency on twilio and made it a regular rest api

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from botting_nigga import ask, update_training_questions
-from twilio.twiml.messaging_response import MessagingResponse
-from flask import Flask, request, session
+from flask import Flask, request, session, jsonify
 app = Flask(__name__)
 # if for some reason your conversation with Botting Nigga gets weird, change the secret key
 app.config['SECRET_KEY'] = '24005770.848dfgb25dvf31ed'
@@ -8,15 +7,15 @@ app.config['SECRET_KEY'] = '24005770.848dfgb25dvf31ed'
 
 @app.route('/bottn_nigga', methods=['POST'])
 def bottn_nigga():
-    incoming_msg = request.values['Body']
+    incoming_msg = request.json['msg']
 
     answer = ask(incoming_msg)
 
     update_training_questions(incoming_msg, answer)
 
-    msg = MessagingResponse()
-    msg.message(answer)
-    return str(msg)
+    
+
+    return jsonify({'msg': answer})
 
 
 if __name__ == '__main__':
