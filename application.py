111d4f1c4e443d316e191c0335f73fd7f4c05063
Nate Baker <natebaker@Nates-MacBook-Pro.local>
2022-11-17 21:30:49 -0500

    renamed the file so it can be ran on aws

--- application.py
+++ application.py
@@ -2,12 +2,12 @@ from botting_nigga import ask, update_training_questions
 from flask import Flask, request, session, jsonify
 from flask_cors import CORS, cross_origin
 from db import get_convo_history, save_entry_into_full_history
-app = Flask(__name__)
+application = Flask(__name__)
 # if for some reason your conversation with Botting Nigga gets weird, change the secret key
-app.config['SECRET_KEY'] = '24005770.848dfgb25dvf31ed'
+application.config['SECRET_KEY'] = '24005770.848dfgb25dvf31ed'
 
 
-@app.route('/bottn_nigga', methods=['POST'])
+@application.route('/bottn_nigga', methods=['POST'])
 @cross_origin()
 def bottn_nigga():
     incoming_msg = request.json['msg']
@@ -24,7 +24,7 @@ def bottn_nigga():
 
     return jsonify({'msg': answer})
 
-@app.route('/convo-history', methods=['GET'])
+@application.route('/convo-history', methods=['GET'])
 @cross_origin()
 def convo_history():
     conversation_history = get_convo_history()
@@ -37,6 +37,6 @@ def convo_history():
 
 
 if __name__ == '__main__':
-    cors = CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    app.run(debug=True)
+    cors = CORS(application)
+    application.config['CORS_HEADERS'] = 'Content-Type'
+    application.run(debug=True)
